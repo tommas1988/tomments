38f2947c461c873a1b6cc91052631d888d100927
Tommas Yuan <tommas1988@live.com>
2014-11-17 23:47:24 +0800

    Modify comments schema

--- example/data/comments.sql
+++ example/data/comments.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
         `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        `parent_id` INT UNSIGNED DEFAULT NULL,
-        `origin_id` INT UNSIGNED DEFAULT NULL,
+        `parent_id` INT UNSIGNED NOT NULL DEFAULT 0,
+        `origin_id` INT UNSIGNED NOT NULL DEFAULT 0,
         `level` TINYINT UNSIGNED NOT NULL DEFAULT 0,
-        `child_count` TINYINT UNSIGNED DEFAULT NULL,
+        `child_count` TINYINT UNSIGNED NOT NULL DEFAULT 0,
         `target_id` INT UNSIGNED NOT NULL,
         `content` TEXT NOT NULL,
-        `time` DATETIME NOT NULL,
+        `time` TIMESTAMP NOT NULL DEFAULT 0,
         `state` TINYINT UNSIGNED DEFAULT 1
 ) ENGINE InnoDB CHARACTER SET UTF8;
